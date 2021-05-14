251f0103d6b67604b6c75eb0ddad603633334262
btomasd <btomasd@gmail.com>
2021-05-14 12:37:57 -0300

    test

--- main_test.go
+++ main_test.go
@@ -1,20 +1,20 @@
-package main
+package main_test
 
 import (
 	"log"
 	"net/http"
 )
 
-type Server struct{}
+type ServerTest struct{}
 
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *ServerTest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(`{"message": "hello world"}`))
 }
 
 func main() {
-	s := &Server{}
+	s := &ServerTest{}
 	http.Handle("/", s)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
